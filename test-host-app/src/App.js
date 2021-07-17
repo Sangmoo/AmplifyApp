4a01c6576398c391d84ea587cb4d2786c0a7cca7
Sangmoo <first_1st@naver.com>
2021-07-18 00:24:42 +0900

    Amplify Host App

--- test-host-app/src/App.js
+++ test-host-app/src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+import { withAuthenticator } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello Sangmoo Amplify World</h1>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
